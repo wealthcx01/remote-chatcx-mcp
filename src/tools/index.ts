9bb8e82b71181825a5dda8138cc400e40f8e1332
wealthcx01 <john.gallagher@wealthcx.com>
2025-08-26 12:11:37 +0100

    Restore worker startup logic and tool exports

---
+++ src/tools/index.ts
@@ -0,0 +1,10 @@
+export { default as get_quote } from "./get_quote";
+export { default as get_timeseries } from "./get_timeseries";
+export { default as get_news } from "./get_news";
+export { default as get_chart } from "./get_chart";
+
+// If you add optional services, uncomment or add these:
+// export { default as get_fundamentals } from "./get_fundamentals";
+// export { default as get_topnews } from "./get_topnews";
+// export { default as get_filings } from "./get_filings";
+// export { default as get_breakingviews } from "./get_breakingviews";
